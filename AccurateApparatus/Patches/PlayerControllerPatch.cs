16a9f3bd6df92b0cb3970a836f614b5b72f864a7
rogan <justsomegiraffes@gmail.com>
2025-06-02 20:36:57 -0400

    Add project files.

---
+++ AccurateApparatus/Patches/PlayerControllerPatch.cs
@@ -0,0 +1,55 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AccurateApparatus.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerPatch
+    {
+        public static float CoroutineTime = 0f;
+        public static PlayerControllerB NetworkController;
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void HoldDamagePatch(ref PlayerControllerB __instance)
+        {
+            if (__instance.currentlyHeldObjectServer != null)
+            {
+                if (__instance.currentlyHeldObjectServer.name.ToString() == "LungApparatus(Clone)" || __instance.currentlyHeldObjectServer.name.ToString() == "LungApparatusTurnedOff(Clone)")
+                {
+                    CoroutineTime += Time.deltaTime;
+
+                    if (CoroutineTime > 1f)
+                    {
+                        if (__instance.health < 10)
+                        {
+                            __instance.bleedingHeavily = true;
+                        }
+
+                        if (__instance.health > 0 && __instance.health < 20)
+                        {
+                            __instance.health -= 2;
+                        }
+                        else if (__instance.health > 0)
+                        {
+                            __instance.health -= 1;
+                        }
+                        else
+                        {
+                            __instance.DamagePlayer(int.MaxValue, true, true, CauseOfDeath.Unknown, 0, false, default);
+                        }
+
+                        HUDManager.Instance.UpdateHealthUI(__instance.health, false);
+
+                        HUDManager.Instance.UIAudio.Stop();
+                        HUDManager.Instance.HUDAnimator.ResetTrigger("SmallHit");
+                        HUDManager.Instance.HUDAnimator.ResetTrigger("CriticalHit");
+
+                        CoroutineTime = 0f;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AccurateApparatus/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using AccurateApparatus.Patches;
+using HarmonyLib;
+
+namespace AccurateApparatus
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class AccurateApparatusBase : BaseUnityPlugin
+    {
+        private const string modGUID = "rogan.AccurateApparatus";
+        private const string modName = "Accurate Apparatus";
+        private const string modVersion = "2.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static AccurateApparatusBase Instance;
+
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("AccurateApparatus Started!");
+
+            harmony.PatchAll(typeof(AccurateApparatusBase));
+            harmony.PatchAll(typeof(PlayerControllerPatch));
+        }
+    }
+}
