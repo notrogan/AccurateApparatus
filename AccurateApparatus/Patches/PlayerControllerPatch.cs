4b59d3c460bf3d9da9d2716e7022ea742f8e0044
rogan <justsomegiraffes@gmail.com>
2025-07-16 02:26:50 -0400

    fix literally everything

--- AccurateApparatus/Patches/PlayerControllerPatch.cs
+++ AccurateApparatus/Patches/PlayerControllerPatch.cs
@@ -1,5 +1,6 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
+using System;
 using UnityEngine;
 
 namespace AccurateApparatus.Patches
@@ -7,46 +8,76 @@ namespace AccurateApparatus.Patches
     [HarmonyPatch(typeof(PlayerControllerB))]
     internal class PlayerControllerPatch
     {
-        public static float CoroutineTime = 0f;
         public static PlayerControllerB NetworkController;
+
+        public static int ClientId;
+        public static string PlayerUsername;
+
+        public static int Health = 0; 
+        public static float Timer = 0f;
 
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         public static void HoldDamagePatch(ref PlayerControllerB __instance)
         {
-            if (__instance.currentlyHeldObjectServer != null)
+            if (GameNetworkManager.Instance.localPlayerController != null && GameNetworkManager.Instance.localPlayerController != null)
             {
-                if (__instance.currentlyHeldObjectServer.name.ToString() == "LungApparatus(Clone)" || __instance.currentlyHeldObjectServer.name.ToString() == "LungApparatusTurnedOff(Clone)")
+                NetworkController = GameNetworkManager.Instance.localPlayerController;
+
+                PlayerUsername = NetworkController.playerUsername;
+                ClientId = (int)NetworkController.playerClientId;
+
+                if (!NetworkController.isPlayerDead)
                 {
-                    CoroutineTime += Time.deltaTime;
-
-                    if (CoroutineTime > 1f)
+                    if (StartOfRound.Instance.inShipPhase == false)
                     {
-                        if (__instance.health < 10)
+                        if (__instance.playerUsername == PlayerUsername)
                         {
-                            __instance.bleedingHeavily = true;
-                        }
+                            if (__instance.currentlyHeldObjectServer != null)
+                            {
+                                if (__instance.currentlyHeldObjectServer.name.ToString() == "LungApparatus(Clone)" || __instance.currentlyHeldObjectServer.name.ToString() == "LungApparatusTurnedOff(Clone)")
+                                {
+                                    Timer += Time.fixedDeltaTime / 2.05f;
 
-                        if (__instance.health > 0 && __instance.health < 20)
-                        {
-                            __instance.health -= 2;
-                        }
-                        else if (__instance.health > 0)
-                        {
-                            __instance.health -= 1;
-                        }
-                        else
-                        {
-                            __instance.DamagePlayer(int.MaxValue, true, true, CauseOfDeath.Unknown, 0, false, default);
-                        }
+                                    if (Timer > 1.7f)
+                                    {
+                                        Health = __instance.health;
+                                        AccurateApparatusBase.mls.LogInfo(Health);
 
-                        HUDManager.Instance.UpdateHealthUI(__instance.health, false);
+                                        if (Health == 0)
+                                        {
+                                            __instance.DamagePlayer(int.MaxValue, true, true, CauseOfDeath.Unknown, 0, false, default);
+                                            return;
+                                        }
 
-                        HUDManager.Instance.UIAudio.Stop();
-                        HUDManager.Instance.HUDAnimator.ResetTrigger("SmallHit");
-                        HUDManager.Instance.HUDAnimator.ResetTrigger("CriticalHit");
+                                        if (Health < 10)
+                                        {
+                                            __instance.bleedingHeavily = true;
+                                        } 
+                                        
+                                        if (Health > 0 && Health <= 20)
+                                        {
+                                            __instance.health -= 3;
+                                        }
+                                        else
+                                        {
+                                            __instance.health -= 1;
+                                        }
 
-                        CoroutineTime = 0f;
+                                        Timer = 0f;
+
+                                        HUDManager.Instance.UIAudio.Stop();
+                                        HUDManager.Instance.HUDAnimator.ResetTrigger("SmallHit");
+                                        HUDManager.Instance.HUDAnimator.ResetTrigger("CriticalHit");
+                                        HUDManager.Instance.UpdateHealthUI(__instance.health, false);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        __instance.bleedingHeavily = false;
                     }
                 }
             }
